a10ccbefcecd25cb66e8707be739813ace9b9cab
neuromancer402 <neuromancer402@yandex.ru>
2025-08-29 17:51:44 +0300

    список записей без подтверждения

--- Scripts/MasterScript.js
+++ Scripts/MasterScript.js
@@ -1,15 +1,22 @@
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
+const dbController = require("./dbController");
+const messageContent = require("../BotData/messageContent.json");
+
 
 function begin (ctx){
-    require("./dbController").master.set.full({
+    dbController.master.set.full({
         tg_id: ctx.message.from.id,
         tg_username: ctx.message.from.username,
         tg_chat_id: ctx.message.chat.id
     }).then(async ()=>{
         try{
             ctx.reply(await getCurrentRecords(ctx));
-            ctx.reply(await getUnconfirmedRegistration(ctx))
+            ctx.reply(await getUnconfirmedRegistration(ctx),
+                {
+                    parse_mode: 'MarkdownV2'
+                }
+            );
         }
         catch(e){
             throw e;
@@ -30,11 +37,11 @@ async function getCurrentRecords(ctx){
             name = element.name;
         }
     });
-    const list = await require("./dbController").procedure_schedule.get.allActiveByMasterUsername(ctx.message.from.username);
-    let answer = require("../BotData/messageContent.json").master.noRegistration;
+    const list = await dbController.procedure_schedule.get.allActiveByMasterUsername(ctx.message.from.username);
+    let answer = messageContent.master.noRegistration;
     
     if(list != null && list.lenth>0){
-        answer = require("../BotData/messageContent.json").master.haveRegistration + JSON.stringify(list);
+        answer = messageContent.master.haveRegistration + JSON.stringify(list);
     }
     return await sayHello(name)+"\n\n"+answer;
 }
@@ -44,8 +51,21 @@ function sayHello(name){
 }
 
 async function getUnconfirmedRegistration(ctx){
-    const list = await require("../Scripts/dbController").procedure_schedule.get.allUnconfirmed();
-    let answer = require("../BotData/messageContent.json").master.haveRegistration+"\n";
-    const procedures = await require("../Scripts/dbController").procedures.get.all();
+    const list = await dbController.procedure_schedule.get.allUnconfirmed();
+    let answer = messageContent.master.haveRegistration+"\n";
+    const procedures = await dbController.procedures.get.all();
+    let num=0;
+    list.listPS.forEach(registration => {
+        for(let i in list.clients){
+            if(list.clients[i].id == registration.clients_id){
+                for(let j in procedures){
+                    if(procedures[j].id == registration.procedures_id){
+                        num++;
+                        answer += `${num}\\. [${list.clients[i].first_name}](tg://user?id=${list.clients[i].tg_id}) ждет подтверждения записи на ${procedures[j].title}\n`
+                    }
+                }
+            }
+        }
+    });
     return answer;
 }
